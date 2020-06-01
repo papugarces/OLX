05f1758f61e88e42c1d3756b10e1b70166459f9f
JoseMCruz <joscruz09@gmail.com>
2020-06-01 00:22:27 -0500

    Comienzo implementación del Spring Security

--- src/main/java/co/com/eam/avanzada/repository/IUsuarioRepository.java
+++ src/main/java/co/com/eam/avanzada/repository/IUsuarioRepository.java
@@ -1,6 +1,8 @@
 package co.com.eam.avanzada.repository;
 
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,6 +11,10 @@ import co.com.eam.avanzada.domain.Usuario;
 @Repository
 
 public interface IUsuarioRepository extends CrudRepository<Usuario , String> {
+	
+	
+	public Optional<Usuario> findByCorreo(String correo);
+	
 	
 }
                                        
---
+++ src/main/java/co/com/eam/avanzada/service/UserDetailsServiceImpl.java
@@ -0,0 +1,45 @@
+package co.com.eam.avanzada.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import co.com.eam.avanzada.domain.Usuario;
+import co.com.eam.avanzada.repository.IUsuarioRepository;
+import co.com.springboot.entity.Authority;
+
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    IUsuarioRepository userRepository;
+	
+    @Override
+     public UserDetails loadUserByUsername(String correo) throws UsernameNotFoundException {
+		
+     //Buscar el usuario con el repositorio y si no existe lanzar una exepcion
+     Usuario appUser = 
+                 userRepository.findByCorreo(correo).orElseThrow(() -> new UsernameNotFoundException("No existe usuario"));
+		
+    //Mapear nuestra lista de Authority con la de spring security 
+    List grantList = new ArrayList();
+    for (Authority authority: appUser.getAuthority()) {
+        // ROLE_USER, ROLE_ADMIN,..
+        GrantedAuthority grantedAuthority = new SimpleGrantedAuthority(authority.getAuthority());
+            grantList.add(grantedAuthority);
+    }
+		
+    //Crear El objeto UserDetails que va a ir en sesion y retornarlo.
+    UserDetails user = (UserDetails) new User(appUser.getCorreo()(), appUser.getPassword(), grantList);
+         return user;
+    }
+}
